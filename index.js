274f5fcca82fc8b89bf2b02eda572f2f09d17c8a
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-22 18:08:49 +0000

    Connected backend to DB

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 const app = express()
 
 app.use(express.json())
@@ -9,79 +10,81 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         date: "2019-05-30T17:30:31.098Z",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only Javascript",
+//         date: "2019-05-30T18:39:34.091Z",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         date: "2019-05-30T19:20:14.298Z",
+//         important: true
+//     }
+// ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find((note) => note.id === id)
-    if (note) {
-        response.json(note)
-    }
-    else {
-        response.status(404).end()
-    }
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+  if (note) {
+    response.json(note)
+  }
+  else {
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
+  response.status(204).end()
 })
 
 const generateID = () => {
-    const maxID = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxID + 1
+  const maxID = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxID + 1
 }
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if(!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id : generateID(),
-    }
-    
-    notes = notes.concat(note)
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateID(),
+  }
 
-    response.json(note)
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = process.env.PORT || 3001
--- mongo.js
+++ mongo.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
+// if (process.argv.length < 3) {
+//   console.log('Please provide the password as an argument: node mongo.js <password>')
+//   process.exit(1)
+// }
+// const password = process.argv[2]
 
-const password = process.argv[2]
+const password = process.env.PASSWORD
 
 const url =
   `mongodb+srv://fullstack:${password}@cluster0.nthtw.mongodb.net/note-app?retryWrites=true&w=majority`
