be7d23692b80512b788c4939870e1c542f578d71
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-24 19:28:12 +0000

    part 4-1: changed project structure

--- index.js
+++ index.js
@@ -1,97 +1,10 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-require('dotenv').config()
-const Note = require('./models/note')
-const app = express()
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(morgan('tiny'))
-app.use(cors())
+const server = http.createServer(app)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+server.listen(config.PORT, () => {
+  logger.info(`Server running on ${config.PORT}`)
 })
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      }
-      else {
-        response.status(404).end()
-      }
-    })
-    .catch(err => next(err))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status.apply(204).end()
-    })
-    .catch(err => next(err))
-})
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-
-  note
-    .save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => response.json(savedAndFormattedNote))
-    .catch(err => next(err))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(err => next(err))
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-// handler of requests with unknown endpoint
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-  else if (error.name === 'ValidationError') {
-    return response.status(400).send({ error: error.message })
-  }
-
-  next(error)
-}
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
