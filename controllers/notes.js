969819bd88bce9e034112995fa17e35d2fffab9b
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-07 10:38:02 +0100

    part 4-5: till optimising the beforeEach function

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,23 +6,16 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
-
-    if (note) {
-      response.json(note)
-    }
-    else {
-      response.status(404).end()
-    }
-  }
-  catch (e) {
-    next(e)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -31,22 +24,14 @@ notesRouter.post('/', async (request, response, next) => {
     date: new Date(),
   })
 
-  try {
-    const savedNote = await note.save()
-    response.json(savedNote)
-  } catch (e) {
-    next(e)
-  }
+  const savedNote = await note.save()
+  response.json(savedNote)
+
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  }
-  catch (e) {
-    next(e)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -57,7 +42,7 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
     .then(updatedNote => {
       response.json(updatedNote)
     })
