a46dcf668292667dd56892ac1830fe7bc822ba0c
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-08 10:57:08 +0100

    part 4-9: added token authentication

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,6 +21,11 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message })
   }
+  else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  }
 
   next(error)
 }
